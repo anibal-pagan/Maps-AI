35aa88a689cb9fa3186a1529844a3da8a8e54c23
Estefania Torres <estefania.torres1@upr.edu>
2020-05-07 10:44:22 -0400

    Created map with csv input.

---
+++ Maps AI/src/Location.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+
+public class Location {
+
+
+    private boolean isGoal = false;
+    private boolean isStart = false;
+    private boolean isVisited = false;
+    private String name;
+    private HashMap<Location, int[]> neighbors;
+
+    public Location(String name){
+        this.neighbors = new HashMap<>();
+        this.name = name;
+    }
+
+    private String getName(){
+        return name;
+    }
+
+    public void setIsStart(){
+        isStart = true;
+    }
+
+    public void setIsGoal(){
+        isGoal = true;
+    }
+
+    public boolean isVisited(){
+        return  isVisited;
+    }
+
+    public void visit(){
+        isVisited = true;
+    }
+
+    public void addNeighbor(Location location, int distance, int maxSpeed, int averageSpeed){
+
+        int[] ETAvariables = {distance, maxSpeed, averageSpeed};
+        neighbors.put(location, ETAvariables);
+    }
+
+    public HashMap<Location, int[]> getNeighbors(){
+        return neighbors;
+    }
+
+    public String getNeighborsString(){
+        StringBuilder str = new StringBuilder();
+        for(Location l: neighbors.keySet()){
+            str.append(l.getName()+"  ");
+        }
+        return str.toString();
+    }
+}
--- Maps AI/src/Main.java
+++ Maps AI/src/Main.java
@@ -1,5 +1,77 @@
-import java.net.http.*;
+import jdk.internal.cmm.SystemResourcePressureImpl;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
-	
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the path of the csv file containing the map");
+
+        String csvFile = scan.nextLine();
+        HashMap<String, Location> map = new HashMap<>();
+
+        readFile(csvFile, map);
+
+        System.out.println(map.size());
+
+        for(String area: map.keySet()){
+
+            System.out.println(area +" -> "+ map.get(area).getNeighborsString());
+        }
+
+
+
+    }
+
+    private static void readFile(String filePath, HashMap<String, Location> map){
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filePath));
+            String line;
+            while ((line=br.readLine()) != null){
+
+                String[] fromTo = line.split(",");
+                String fromPin = fromTo[0];
+                String toPin = fromTo[1];
+                int distance = Integer.parseInt(fromTo[2]);
+                int maxSpeed = Integer.parseInt(fromTo[3]);
+                int averageSpeed = Integer.parseInt(fromTo[4]);
+
+
+
+                if(map.size()>0 && map.containsKey(fromPin) && map.containsKey(toPin)){
+                    map.get(fromPin).addNeighbor(map.get(toPin),distance, maxSpeed, averageSpeed);
+                    map.get(toPin).addNeighbor(map.get(fromPin), distance, maxSpeed, averageSpeed);
+                }else if(map.size()>0 && map.containsKey(fromPin) && !map.containsKey(toPin)){
+                    Location newLocation= new Location(toPin);
+                    map.put(toPin.trim(), newLocation);
+                    map.get(fromPin).addNeighbor(newLocation, distance, maxSpeed, averageSpeed);
+                    map.get(toPin).addNeighbor(map.get(fromPin), distance, maxSpeed, averageSpeed);
+
+                } else if (map.size()>0 && map.containsKey(toPin) && !map.containsKey(fromPin)){
+                    Location newLocation= new Location(fromPin);
+                    map.put(fromPin.trim(), newLocation);
+                    map.get(toPin).addNeighbor(newLocation, distance, maxSpeed, averageSpeed);
+                    newLocation.addNeighbor(map.get(toPin), distance, maxSpeed, averageSpeed);
+                } else {
+                    Location from = new Location(fromPin);
+                    map.put(fromPin, from);
+                    Location to = new Location(toPin);
+                    map.put(toPin, to);
+                    map.get(fromPin).addNeighbor(to, distance, maxSpeed, averageSpeed);
+                    map.get(toPin).addNeighbor(from,distance, maxSpeed,averageSpeed);
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
